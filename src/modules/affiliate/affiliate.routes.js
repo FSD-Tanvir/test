b78f8c0cce81434a77f34718de38a8c1b5bea541
Agency Ltd <agency@example.com>
2025-01-30 14:53:14 +0600

    affiliate module updated

--- src/modules/affiliate/affiliate.routes.js
+++ src/modules/affiliate/affiliate.routes.js
@@ -2,19 +2,25 @@ const express = require("express");
 const router = express.Router();
 const {
 	createAffiliate,
-	clickUpdate,
 	getAffiliateDataHandler,
 	getAllAffiliatesHandler,
 	updateAffiliateHandler,
 	getAffiliate
 } = require("./affiliate.controller.js");
 
+// Route to handle POST requests for creating an affiliate
 router.post("/create", createAffiliate);
+
+// Route to handle GET requests for all affiliates
 router.get("/", getAllAffiliatesHandler);
+
+// Route to handle GET requests for affiliate info
 router.get("/affiliate-info/:email", getAffiliateDataHandler);
+
 // Route to get affiliate by referral code
 router.get('/:referralCode', getAffiliate);
-router.get("/ref/:referralCode", clickUpdate);
+
+// Route to handle PUT requests for updating an affiliate
 router.put("/affiliate-update/:id", updateAffiliateHandler);
 
 module.exports = router;
