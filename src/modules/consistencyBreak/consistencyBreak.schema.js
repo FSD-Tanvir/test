4a90f6132a132be5c37739fe60aae626f00c8987
Agency Ltd <agency@example.com>
2025-02-26 17:47:16 +0600

    ♻️  disable email template updated

--- src/modules/consistencyBreak/consistencyBreak.schema.js
+++ src/modules/consistencyBreak/consistencyBreak.schema.js
@@ -1,50 +1,54 @@
 const mongoose = require("mongoose");
 
 const consistencyBreakSchema = new mongoose.Schema(
-	{
-		account: {
-			type: Number,
-			required: true,
-		},
-		accountSize: {
-			type: Number,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		ticket: {
-			type: Number,
-			required: true,
-			unique: true,
-		},
-		profit: {
-			type: Number,
-			required: true,
-		},
-		profitPercentage: {
-			type: Number,
-			required: true,
-		},
-		profitDifference: {
-			type: Number,
-			required: true,
-		},
+    {
+        account: {
+            type: Number,
+            required: true,
+        },
+        accountSize: {
+            type: Number,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        ticket: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
+        profit: {
+            type: Number,
+            required: true,
+        },
+        profitPercentage: {
+            type: Number,
+            required: true,
+        },
+        profitDifference: {
+            type: Number,
+            required: true,
+        },
         profitLimit: {
             type: Number,
             required: true,
         },
-		emailSent: {
-			type: Boolean,
-			default: false,
-		},
-		isDisabled: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	{ timestamps: true }
+        emailSent: {
+            type: Boolean,
+            default: false,
+        },
+        emailCount: {
+            type: Number,
+            default: 0, // Track the number of emails sent
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("ConsistencyBreak", consistencyBreakSchema);
