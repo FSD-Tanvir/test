2f47dd3b4bd7bff0d9724a59063081d14bb9c038
Agency Ltd <agency@example.com>
2025-02-27 12:17:12 +0600

    ⚡️  cron job added in allFunctions

--- src/riskMangement/allfunction.js
+++ src/riskMangement/allfunction.js
@@ -1,6 +1,25 @@
 const { storeDailyDataController } = require("../modules/breach/breach.controller");
+const { consistencyBreak } = require("../modules/consistencyBreak/consistencyBreak.services");
+const { lotSizeRisk } = require("../modules/lotSizeRisk/lotSizeRisk.services");
+const { stopLossRisk } = require("../modules/stopLossRisk/stopLossRisk.services");
+const cron = require("node-cron");
 
 const runAllFunctions = () => {
     storeDailyDataController();
+
+    // Schedule lotSizeRisk to run daily at 21:30
+    cron.schedule("30 21 * * *", () => {
+        lotSizeRisk();
+    });
+
+    // Schedule stopLossRisk to run at a specific time (e.g., 22:00)
+    cron.schedule("00 22 * * *", () => {
+        stopLossRisk();
+    });
+
+    // Schedule consistencyBreak to run 30 minutes after stopLossRisk (e.g., 22:30)
+    cron.schedule("30 22 * * *", () => {
+        consistencyBreak();
+    });
 };
 module.exports = { runAllFunctions };
