705d2a0382ec487ce0c7e7b5f5a7e2f98ff919c2
Agency Ltd <agency@example.com>
2025-03-05 14:51:00 +0600

    ♻️ Add automated stop loss email functionality and schedule tasks for risk management

--- src/riskMangement/allfunction.js
+++ src/riskMangement/allfunction.js
@@ -1,39 +1,46 @@
 const { storeDailyDataController } = require("../modules/breach/breach.controller");
 const {
-    consistencyBreak,
-    sendAutomatedConsistencyBreakEmail,
+	consistencyBreak,
+	sendAutomatedConsistencyBreakEmail,
 } = require("../modules/consistencyBreak/consistencyBreak.services");
 const { lotSizeRisk } = require("../modules/lotSizeRisk/lotSizeRisk.services");
 const {
-    checkAndSaveInactiveAccounts,
+	checkAndSaveInactiveAccounts,
 } = require("../modules/sevenDaysTradingChallenge/sevenDaysTradingChallenge.controller");
-const { stopLossRisk } = require("../modules/stopLossRisk/stopLossRisk.services");
+const {
+	stopLossRisk,
+	sendAutomatedStopLossEmail,
+} = require("../modules/stopLossRisk/stopLossRisk.services");
 const cron = require("node-cron");
 
 const runAllFunctions = () => {
-    storeDailyDataController();
+	storeDailyDataController();
 
-    // Schedule lotSizeRisk to run daily at 21:30
-    cron.schedule("30 21 * * *", () => {
-        lotSizeRisk();
-    });
+	// Schedule lotSizeRisk to run daily at 21:30
+	cron.schedule("30 21 * * *", () => {
+		lotSizeRisk();
+	});
 
-    // Schedule stopLossRisk to run at a specific time (e.g., 22:00)
-    cron.schedule("00 22 * * *", () => {
-        stopLossRisk();
-    });
+	// Schedule stopLossRisk to run at a specific time (e.g., 22:00)
+	cron.schedule("00 22 * * *", () => {
+		stopLossRisk();
+	});
 
-    // Schedule consistencyBreak to run 30 minutes after stopLossRisk (e.g., 22:30)
-    cron.schedule("30 22 * * *", () => {
-        consistencyBreak();
-    });
+	// Schedule consistencyBreak to run 30 minutes after stopLossRisk (e.g., 22:30)
+	cron.schedule("30 22 * * *", () => {
+		consistencyBreak();
+	});
 
-    cron.schedule("45 12 * * *", () => {
-        checkAndSaveInactiveAccounts();
-    });
+	cron.schedule("45 12 * * *", () => {
+		checkAndSaveInactiveAccounts();
+	});
 
-    cron.schedule("50 12 * * *", () => {
-        sendAutomatedConsistencyBreakEmail();
-    });
+	cron.schedule("50 22 * * *", () => {
+		sendAutomatedConsistencyBreakEmail();
+	});
+
+	cron.schedule("10 23 * * *", () => {
+		sendAutomatedStopLossEmail();
+	});
 };
 module.exports = { runAllFunctions };
