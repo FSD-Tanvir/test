ffb4b2da264319f26fb3bbeded235074ff8dc23b
Agency Ltd <agency@example.com>
2025-03-19 15:53:31 +0600

    💡 necessary comments added in all function

--- src/riskMangement/allfunction.js
+++ src/riskMangement/allfunction.js
@@ -1,60 +1,65 @@
+const { storeDailyDataController } = require("../modules/breach/breach.controller");
 const {
-  storeDailyDataController,
-} = require("../modules/breach/breach.controller");
-const {
-  consistencyBreak,
-  sendAutomatedConsistencyBreakEmail,
+    consistencyBreak,
+    sendAutomatedConsistencyBreakEmail,
 } = require("../modules/consistencyBreak/consistencyBreak.services");
 const {
-  lotSizeRisk,
-  sendAutomatedLotSizeEmail,
+    lotSizeRisk,
+    sendAutomatedLotSizeEmail,
 } = require("../modules/lotSizeRisk/lotSizeRisk.services");
 const {
-  checkAndSaveInactiveAccounts,
+    checkAndSaveInactiveAccounts,
 } = require("../modules/sevenDaysTradingChallenge/sevenDaysTradingChallenge.controller");
 const {
-  stopLossRisk,
-  sendAutomatedStopLossEmail,
+    stopLossRisk,
+    sendAutomatedStopLossEmail,
 } = require("../modules/stopLossRisk/stopLossRisk.services");
 const cron = require("node-cron");
-const {sendAutomatedTwoPercentEmail,} = require("../modules/twoPercentRisk/twoPercentRisk.services");
+const {
+    sendAutomatedTwoPercentEmail,
+} = require("../modules/twoPercentRisk/twoPercentRisk.services");
 
 const runAllFunctions = () => {
-  storeDailyDataController();
+    storeDailyDataController();
 
-  // Schedule lotSizeRisk to run daily at 21:30
-  cron.schedule("30 21 * * *", () => {
-    lotSizeRisk();
-  });
+    // Schedule lotSizeRisk to run daily at 21:30
+    cron.schedule("30 21 * * *", () => {
+        lotSizeRisk();
+    });
 
-  // Schedule stopLossRisk to run at a specific time (e.g., 22:00)
-  cron.schedule("00 22 * * *", () => {
-    stopLossRisk();
-  });
+    // Schedule stopLossRisk to run at a specific time (e.g., 22:00)
+    cron.schedule("00 22 * * *", () => {
+        stopLossRisk();
+    });
 
-  // Schedule consistencyBreak to run 30 minutes after stopLossRisk (e.g., 22:30)
-  cron.schedule("30 22 * * *", () => {
-    consistencyBreak();
-  });
+    // Schedule consistencyBreak to run 30 minutes after stopLossRisk (e.g., 22:30)
+    cron.schedule("30 22 * * *", () => {
+        consistencyBreak();
+    });
 
-  cron.schedule("45 12 * * *", () => {
-    checkAndSaveInactiveAccounts();
-  });
+    // Schedule checkAndSaveInactiveAccounts to run daily at 12:45
+    cron.schedule("45 12 * * *", () => {
+        checkAndSaveInactiveAccounts();
+    });
 
-  cron.schedule("50 22 * * *", () => {
-    sendAutomatedConsistencyBreakEmail();
-  });
+    // Schedule sendAutomatedConsistencyBreakEmail to run daily at 22:50
+    cron.schedule("50 22 * * *", () => {
+        sendAutomatedConsistencyBreakEmail();
+    });
 
-  cron.schedule("10 23 * * *", () => {
-    sendAutomatedStopLossEmail();
-  });
+    // Schedule sendAutomatedStopLossEmail to run every 6 hours
+    cron.schedule("0 */6 * * *", () => {
+        sendAutomatedStopLossEmail();
+    });
 
-  cron.schedule("25 23 * * *", () => {
-    sendAutomatedLotSizeEmail();
-  });
+    // Schedule lotSizeRisk to run daily at 23:25
+    cron.schedule("25 23 * * *", () => {
+        sendAutomatedLotSizeEmail();
+    });
 
-  cron.schedule("19 0 * * *", () => {
-    sendAutomatedTwoPercentEmail();
-  });
+    // Schedule sendAutomatedTwoPercentEmail to run daily at 00:19
+    cron.schedule("19 0 * * *", () => {
+        sendAutomatedTwoPercentEmail();
+    });
 };
 module.exports = { runAllFunctions };
