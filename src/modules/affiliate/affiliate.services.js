0fe644968b5c03dde04c7d7d36c06a9bb5c42651
Agency Ltd <agency@example.com>
2025-04-05 12:21:44 +0600

    updated

--- src/modules/affiliate/affiliate.services.js
+++ src/modules/affiliate/affiliate.services.js
@@ -1,10 +1,108 @@
 // services/affiliate.services.js
-const { sendEmailSingleRecipient } = require("../../helper/mailing.js");
 const MAffiliate = require("./affiliate.schema.js");
+const nodemailer = require("nodemailer");
+const fs = require("node:fs");
+const path = require("node:path");
+
+
 
 const createAffiliate = async (affiliateData) => {
 	const affiliate = new MAffiliate(affiliateData);
-	return await affiliate.save();
+
+	const savedAffiliateRequest = await affiliate.save();
+
+	const htmlTemplate = `
+	<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 2px solid #DB8112; border-radius: 10px; background-color: #ffffff; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+	  <div style="text-align: center; margin-bottom: 15px;">
+		<img src="https://i.ibb.co.com/34qjbqp/Fox-Funded-Logo.png" alt="Company Logo" style="max-width: 120px; height: auto;">
+	  </div>
+  
+	  <p style="font-size: 16px; color: #333; text-align: left; margin-bottom: 10px;">
+		Hi Admin,
+	  </p>
+  
+	  <p style="font-size: 16px; color: #333; text-align: left; margin-bottom: 10px;">
+		You have received a new affiliate request. Please find the details below:
+	  </p>
+  
+	  <p style="font-size: 16px; color: #333; text-align: left; margin-bottom: 5px;">
+		<strong>Email:</strong> ${savedAffiliateRequest?.email}
+	  </p>
+  
+	  <p style="font-size: 16px; color: #333; text-align: left; margin-bottom: 20px;">
+		<strong>WhatsApp:</strong> ${savedAffiliateRequest?.whatsAppNumber}
+	  </p>
+  
+	  <p style="font-size: 16px; color: #333; text-align: left;">
+		Please review and take the necessary action.
+	  </p>
+  
+	  <p style="font-size: 16px; color: #333; text-align: left; margin-top: 20px;">
+		Best regards,<br>
+		<strong>Foxxx Funded</strong>
+	  </p>
+  	<p style="font-size: 14px; color: #777; margin-top: 20px;">
+		  If you have any questions, feel free to
+		  <a href="https://foxx-funded.com/contact-us" style="color: #DB8112; text-decoration: none; font-weight: bold;">
+			contact our support team
+		  </a>.
+		</p>
+	  <div style="margin-top: 20px; text-align: center;">
+		<a href="https://t.me/+2QVq5aChxiBlOWFk" style="margin-right: 10px;">
+		  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUQ9pRZvmScqICRjNBvAHEjIawnL1erY-AcQ&s" alt="Telegram" style="width: 32px; height: 32px;">
+		</a>
+	  </div>
+	</div>
+  
+	<style>
+	  @media only screen and (max-width: 600px) {
+		div[style] {
+		  padding: 10px !important;
+		}
+		p[style], a[style] {
+		  font-size: 16px !important;
+		}
+	  }
+	</style>
+  `;
+
+	const transporter = nodemailer.createTransport({
+		// service: 'gmail',
+		host: "smtp-relay.brevo.com",
+
+		auth: {
+			user: process.env.user, // Your Gmail address from environment variables
+			pass: process.env.pass, // Your Gmail password from environment variables
+		},
+	});
+
+	const sendEmailSingleRecipientFromCustomer = async (to, subject, text, html = "") => {
+		const mailOptions = {
+			from: savedAffiliateRequest?.email, // sender address
+			// from: process.env.user, // Sender address
+			to, // list of receivers
+			subject, // Subject line
+			text, // plain text body
+			html, // html body
+		};
+
+		try {
+			const info = await transporter.sendMail(mailOptions);
+			return info.response;
+		} catch (error) {
+			throw error;
+		}
+	};
+	if (savedAffiliateRequest) {
+		await sendEmailSingleRecipientFromCustomer(
+			"zentexx2023@gmail.com",
+			`You have received a new affiliate request. ${savedAffiliateRequest.email}`,
+			htmlTemplate
+		);
+	}
+
+
+	return savedAffiliateRequest;
 };
 
 const getAffiliateData = async (email) => {
