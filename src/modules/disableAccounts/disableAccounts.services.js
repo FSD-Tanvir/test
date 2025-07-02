6efd4d464a4c9522364fc74f85160c1bbe9228a1
Agency Ltd <agency@example.com>
2025-07-02 20:55:09 +0600

    up

--- src/modules/disableAccounts/disableAccounts.services.js
+++ src/modules/disableAccounts/disableAccounts.services.js
@@ -1,108 +1,125 @@
 const { sendEmailSingleRecipient } = require("../../helper/mailing.js");
-const { disableMailingHTMLTemplate } = require("../../helper/utils/disableMailingHTMLTemplate.js");
+const {
+  disableMailingHTMLTemplate,
+} = require("../../helper/utils/disableMailingHTMLTemplate.js");
 const { findUserByAccount } = require("../../helper/utils/findUser.js");
-const { DisableAccount } = require("./disableAccounts.schema.js");
+const {
+  DisableAccount,
+  DisableAccountMatchTrader,
+} = require("./disableAccounts.schema.js");
 
 const saveRealTimeLog = async (
-	accountNumber,
-	lossPercentage,
-	asset,
-	balance,
-	initialBalance,
-	equity,
-	message
+  accountNumber,
+  lossPercentage,
+  asset,
+  balance,
+  initialBalance,
+  equity,
+  message
 ) => {
-	// console.log("line 18", accountNumber, lossPercentage, asset, equity, message)
-	try {
-		// Validate that accountNumber is not null or undefined
-		if (!accountNumber) {
-			console.error("Invalid accountNumber:", accountNumber);
-			throw new Error("MT4Account is required and cannot be null or undefined.");
-		}
+  // console.log("line 18", accountNumber, lossPercentage, asset, equity, message)
+  try {
+    // Validate that accountNumber is not null or undefined
+    if (!accountNumber) {
+      console.error("Invalid accountNumber:", accountNumber);
+      throw new Error(
+        "MT4Account is required and cannot be null or undefined."
+      );
+    }
 
-		// Check if an entry with the same MT4Account already exists
-		// biome-ignore lint/style/useConst: <explanation>
-		let existingLog = await DisableAccount.findOne({
-			mt5Account: accountNumber,
-		});
+    // Check if an entry with the same MT4Account already exists
+    // biome-ignore lint/style/useConst: <explanation>
+    let existingLog = await DisableAccount.findOne({
+      mt5Account: accountNumber,
+    });
 
-		if (existingLog) {
-			return {
-				success: false,
-				message: "Log entry with this MT4Account already exists",
-			};
-		}
+    if (existingLog) {
+      return {
+        success: false,
+        message: "Log entry with this MT4Account already exists",
+      };
+    }
 
-		// Create a new log entry if not already existing
-		const newLog = new DisableAccount({
-			mt5Account: accountNumber,
-			lossPercentage: lossPercentage, // Ensure correct value here
-			asset: asset,
-			balance: balance,
-			initialBalance: initialBalance, // Ensure correct value here
-			equity: equity, // Ensure correct value here
-			message: message, // Ensure correct value here
-		});
+    // Create a new log entry if not already existing
+    const newLog = new DisableAccount({
+      mt5Account: accountNumber,
+      lossPercentage: lossPercentage, // Ensure correct value here
+      asset: asset,
+      balance: balance,
+      initialBalance: initialBalance, // Ensure correct value here
+      equity: equity, // Ensure correct value here
+      message: message, // Ensure correct value here
+    });
 
-		// Save the new log entry to the database
-		await newLog.save();
+    // Save the new log entry to the database
+    await newLog.save();
 
-		/*🧲🧲🧲🧲Account bridge or disable informing by mail start point*/
+    /*🧲🧲🧲🧲Account bridge or disable informing by mail start point*/
 
-		const userFromOwn = await findUserByAccount(accountNumber);
-		const { email } = userFromOwn;
-		if (message === "MaxTotalLoss") {
-			const MaxHtml = await disableMailingHTMLTemplate(
-				accountNumber,
-				message,
-				lossPercentage,
-				initialBalance,
-				equity
-			);
-			await sendEmailSingleRecipient(email, "Account Status", message, MaxHtml);
-		} else {
-			const DailyHtml = await disableMailingHTMLTemplate(
-				accountNumber,
-				message,
-				lossPercentage,
-				asset,
-				equity
-			);
-			await sendEmailSingleRecipient(email, "Account Status", message, DailyHtml);
-		}
+    const userFromOwn = await findUserByAccount(accountNumber);
+    const { email } = userFromOwn;
+    if (message === "MaxTotalLoss") {
+      const MaxHtml = await disableMailingHTMLTemplate(
+        accountNumber,
+        message,
+        lossPercentage,
+        initialBalance,
+        equity
+      );
+      await sendEmailSingleRecipient(email, "Account Status", message, MaxHtml);
+    } else {
+      const DailyHtml = await disableMailingHTMLTemplate(
+        accountNumber,
+        message,
+        lossPercentage,
+        asset,
+        equity
+      );
+      await sendEmailSingleRecipient(
+        email,
+        "Account Status",
+        message,
+        DailyHtml
+      );
+    }
 
-		/*🧲🧲🧲🧲Account bridge or disable informing by mail end point*/
+    /*🧲🧲🧲🧲Account bridge or disable informing by mail end point*/
 
-		return { success: true, message: "Log entry saved successfully" };
-	} catch (error) {
-		// console.error("Error saving real-time log entry:", error.message);
-		return {
-			success: false,
-			message: `Failed to save log entry: ${error.message}`,
-		};
-	}
+    return { success: true, message: "Log entry saved successfully" };
+  } catch (error) {
+    // console.error("Error saving real-time log entry:", error.message);
+    return {
+      success: false,
+      message: `Failed to save log entry: ${error.message}`,
+    };
+  }
 };
 
 const getDisabledAccount = async (account) => {
-	try {
-		const disabledAccount = await DisableAccount.findOne({
-			mt5Account: account,
-		});
-		const disabledAccountMatchTrader = await DisableAccountMatchTrader.findOne({
-			account: account,
-		});
+  try {
+    const disabledAccount = await DisableAccount.findOne({
+      mt5Account: account,
+    });
 
-		if (!disabledAccount) {
-			return null;
-		}
+    if (disabledAccount) {
+      return disabledAccount;
+    }
 
-		return disabledAccount;
-	} catch (error) {
-		console.log(error);
-	}
+    const disabledAccountMatchTrader = await DisableAccountMatchTrader.findOne({
+      matchTraderAccount: account,
+    });
+
+    if (disabledAccountMatchTrader) {
+      return disabledAccountMatchTrader;
+    }
+
+    return null;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-	saveRealTimeLog,
-	getDisabledAccount,
+  saveRealTimeLog,
+  getDisabledAccount,
 };
